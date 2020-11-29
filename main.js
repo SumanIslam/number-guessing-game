f1429024b6f3ed79a1d368a5987bf03a37b2fa1e
suman Islam <sumunahmed1416@gmail.com>
2020-11-29 17:58:23 +0600

    some changes

--- main.js
+++ main.js
@@ -1,61 +1,24 @@
 // variables
 let min = 1,
-    max = 10,
-    winningNumber = Math.floor(Math.random() * (max-min +1) + min),
-    guessesLeft = 3;
-console.log(winningNumber);
+  max = 20,
+  winningNumber = Math.floor(Math.random() * (max - min + 1) + min),
+  guessesLeft = 5;
+// console.log(winningNumber);
 
-// Elements
+// selecting elements
 const game = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessInput = document.querySelector('#guess-input'),
-      guessBtn = document.querySelector('#guess-btn'),
-      message = document.querySelector('.message');
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessInput = document.querySelector('#guess-input'),
+  guessBtn = document.querySelector('#guess-btn'),
+  message = document.querySelector('.message');
 
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
 
-// play Again event listener
-game.addEventListener('mousedown', (e) => {
-  if(e.target.className === 'play-again') {
-    window.location.reload();
-  }
-  console.log(e);
-})
-// Event Listener
-guessBtn.addEventListener('click', (e) => {
-  const guess = parseInt(guessInput.value);
-
-  // check if won or not
-  if(guess === winningNumber){
-    // win
-    gameOver(true, `Yahoo!! number ${winningNumber} is correct. You Win!!`);
-  } else {
-    // wrong number
-    guessesLeft--;
-
-    if(guessesLeft === 0) {
-      // game over - lost
-      gameOver(false, `Game Over, You lost. The correct number was ${winningNumber}`);
-      
-    } else {
-      // game continuous - wrong answer
-      guessInput.style.borderColor = 'red';
-      guessInput.value = '';
-      setMessage(`${guess} is not correct, ${guessesLeft} is left`, 'red');
-    }
-  }
-
-  // validation
-  if (isNaN(guess) || guess < min || guess > max) {
-    setMessage(`Please enter a number between ${min} and ${max}`, 'red')
-  }
-
-  e.preventDefault();
-})
-
+//............. function part ..................
+//............. ******************* ..................
 // setMessage function
 function setMessage(msg, color) {
   message.textContent = msg;
@@ -63,7 +26,7 @@ function setMessage(msg, color) {
 };
 
 // gameOver function
-function gameOver(won,msg) {
+function gameOver(won, msg) {
   let color;
   won === true ? color = 'green' : color = 'red';
   guessInput.disabled = true;
@@ -74,3 +37,54 @@ function gameOver(won,msg) {
   guessBtn.value = 'Play Again';
   guessBtn.className += 'play-again';
 }
+
+//guesses left
+const remainingGuess = () => {
+  // wrong number
+  guessesLeft--;
+
+  if (guessesLeft === 0) {
+    // game over - lost
+    gameOver(false, `Game Over, You lost. The correct number was ${winningNumber}`);
+
+  } else {
+    // game continuous - wrong answer
+    guessInput.style.borderColor = 'red';
+    guessInput.value = '';
+  }
+}
+
+//............. event listener part ..................
+//............. ******************* ..................
+// play Again
+game.addEventListener('mousedown', (e) => {
+  if (e.target.className === 'play-again') {
+    window.location.reload();
+  }
+  // console.log(e);
+})
+
+// checking the guess
+guessBtn.addEventListener('click', (e) => {
+  const guess = parseInt(guessInput.value);
+
+  let guessHigh = `${guess} is too high`,
+    guessLow = `${guess} is too low`;
+
+  // check conditions
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    guessInput.value = '';
+  } else if (guess === winningNumber) {
+    // win
+    gameOver(true, `Yahoo!! number ${winningNumber} is correct. You Win 🚀🎉`);
+  } else if (guess < winningNumber) {
+    remainingGuess();
+    setMessage(`${guessLow}, ${guessesLeft} guess is left`, 'red');
+  } else if(guess > winningNumber) {
+    remainingGuess();
+    setMessage(`${guessHigh}, ${guessesLeft} guess is left`, 'red');
+  }
+
+  e.preventDefault();
+})
